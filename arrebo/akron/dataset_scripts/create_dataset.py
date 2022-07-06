a239780384d676013c7da2564a8d2b8c34a52b30
Alkatraz <andreaboetto98@gmail.com>
2022-07-06 16:44:29 +0200

    -aggiunto script per creare il dataset da zero - aggiunto script per appendere i dati - aggiunti script che dovrebbe rimuovere le emergenze inutili, ma che al momento stampa le emrgenze che abbiamo

---
+++ arrebo/akron/dataset_scripts/create_dataset.py
@@ -0,0 +1,29 @@
+from os import listdir
+from os.path import isfile, join
+import csv
+import pandas as pd
+
+files = [f for f in listdir("../scraper_hinge/merged") if
+         isfile(join("../scraper_hinge/merged", f))]  # file da cui prendere i dati
+with open("../dataset_scripts/dataset_hinge.csv", 'a', newline='') as dataset:  # apro il file dataset
+    header = ["TIME_STAMP"]
+    writer = csv.writer(dataset)
+    files_open = []
+    for file in files:
+        header.append(file.split(".")[0])  # creo l'header
+        files_open.append(pd.read_csv(
+            "../scraper_hinge/merged/" + file))  # mi creo una lista di tutti i file da cui leggere in modo da averli gia aperti pronti
+    writer.writerow(header)
+    for i in range(len(files_open[0])):
+        row = list()
+        first = True
+        for file in files_open:
+            if first:  # solo la prima volta devo mettere il timestamp (tanto è uguale per tutti)
+                row.append(file["board time"][i])
+                row.append(file["value"][i])
+                first = False
+            else:
+                row.append(file["value"][i])
+        writer.writerow(row)
+    dataset.close()
+    #TODO: aggiungere eliminazione dei file da merged
